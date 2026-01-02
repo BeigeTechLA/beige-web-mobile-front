f2351073af507c834effa799023da48dd2eac1c4
Punyashree Shetty <punyashree.shetty@gmail.com>
2026-01-02 20:07:53 +0530

    BAWD167 & BAWD-177

---
+++ app/data/projectsData.tsx
@@ -0,0 +1,82 @@
+export const ProjectsData = [
+  {
+    title: "GumGum CEO Podcast Feature",
+    description: "",
+    video: "https://player.vimeo.com/video/1115246854",
+  },
+  {
+    title: "Colbie Caillat Live Concert Coverage",
+    description: "",
+    video: "https://player.vimeo.com/video/1003494735",
+  },
+  {
+    title: "Pressa – “5K for the Draco” Music Video",
+    description: "",
+    video: "https://player.vimeo.com/video/1150926593",
+  },
+  {
+    title: "The #1 Dad Book – Book Launch Feature",
+    description: "",
+    video: "https://player.vimeo.com/video/1081922454",
+  },
+  {
+    title: "Phil & Kat – Wedding Film",
+    description: "",
+    video: "https://player.vimeo.com/video/460738170",
+  },
+  {
+    title: "Bala Enzyme Social Ad",
+    description: "",
+    video: "https://player.vimeo.com/video/460732074",
+  },
+  {
+    title: "DHL – Training & Enablement Video",
+    description: "",
+    video: "https://player.vimeo.com/video/975604600",
+  },
+  {
+    title: "Thomas – Ultra Marathon Short Film",
+    description: "",
+    video: "https://player.vimeo.com/video/1067856067",
+  },
+  {
+    title: "MAACO x Aaron Robinson Commercial",
+    description: "",
+    video: "https://player.vimeo.com/video/809983749",
+  },
+  {
+    title: "Natasha’s Book Launch Party",
+    description: "",
+    video: "https://player.vimeo.com/video/810765519",
+  },
+  {
+    title: "Walmart Community Health Brand Moment",
+    description: "",
+    video: "https://player.vimeo.com/video/1076183472",
+  },
+  {
+    title: "Baskin Robbins Social Impact Event",
+    description: "",
+    video: "https://player.vimeo.com/video/1054175234",
+  },
+  {
+    title: "WellHub Corporate Retreat",
+    description: "",
+    video: "https://player.vimeo.com/video/1055310221",
+  },
+  {
+    title: "Bear Robotics Brand Spot",
+    description: "",
+    video: "https://player.vimeo.com/video/1006844025",
+  },
+  {
+    title: "Rolls Royce Factory Commercial",
+    description: "",
+    video: "https://player.vimeo.com/video/1079988477",
+  },
+  {
+    title: "Association of Public Charter Schools – Awareness Campaign",
+    description: "",
+    video: "https://player.vimeo.com/video/1122981015",
+  },
+];
