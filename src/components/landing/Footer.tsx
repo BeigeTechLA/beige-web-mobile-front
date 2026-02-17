85749318b96c55ad3950c76ae452b543409cad7d
Punyashree Shetty <punyashree.shetty@gmail.com>
2026-02-17 10:55:33 +0530

    Instagram link in footer

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -46,7 +46,7 @@ const SOCIAL_PLATFORMS = [
   { name: "Facebook", icon: Facebook, href: "https://www.facebook.com/beigemedia" },
   { name: "Linkedin", icon: Linkedin, href: "https://www.linkedin.com/company/beigemedia" },
   // { name: "Youtube", icon: Youtube, href: "https://www.youtube.com/@beigemedia" },
-  // { name: "Instagram", icon: Instagram, href: "https://www.instagram.com/beigemedia" },
+  { name: "Instagram", icon: Instagram, href: "https://www.instagram.com/beige.ai/" },
   // { name: "Twitter", icon: Twitter, href: "https://www.twitter.com/beigemedia" },
   { name: "Tiktok", icon: Music2, href: "https://www.tiktok.com/@beigemedia" },
 ];
