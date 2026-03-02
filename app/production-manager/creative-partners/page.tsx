87833ab3db06d0e7870655e84e13ff5b297feed4
Jaimin Patel <jaiminpatel.2309@gmail.com>
2026-03-02 17:23:09 +0530

    feat: add PMCreativePartnersTable component for managing creative partners

---
+++ app/production-manager/creative-partners/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { PMCreativePartnersTable } from "@/components/production-manager/users/PMCreativePartnersTable";
+
+export default function CreativePartnersPage() {
+    return (
+        <div className="p-4 lg:p-8">
+            <PMCreativePartnersTable />
+        </div>
+    );
+}
