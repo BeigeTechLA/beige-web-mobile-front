f55614c1e3a7236119e6e254092e7ed5f1a2a5a7
Harsh Panchal <harshpanchal7979@gmail.com>
2026-06-25 18:03:00 +0530

    feat: added the Client Archive And Restore Functionality with api intigration with three diffrent tabs. Add: Added archive history section inside the Client[id].

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -58,9 +58,9 @@ const menuItems = [
   },
   { name: 'Finances', icon: DollarSign, 
     children: [
-      // { name: 'Payouts', link: '/admin/finances/payouts' },
-      // { name: 'Transactions', link: '/admin/finances/transactions' },
-      // { name: 'Disputes', link: '/admin/finances/disputes' },
+      { name: 'Payouts', link: '/admin/finances/payouts' },
+      { name: 'Transactions', link: '/admin/finances/transactions' },
+      { name: 'Disputes', link: '/admin/finances/disputes' },
       { name: 'Beige credit points', link: '/admin/finances/creditPoints' },
 
     ] },
