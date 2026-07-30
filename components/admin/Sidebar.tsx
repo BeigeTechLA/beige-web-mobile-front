33838630623e9025224a22b5b2e1a621f323686b
Harsh Panchal <harshpanchal7979@gmail.com>
2026-07-30 18:44:54 +0530

    feat: Comment out 'Transactions' and 'Disputes' menu items in Sidebar components

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -62,8 +62,8 @@ const menuItems = [
   { name: 'Finances', icon: DollarSign, permissionKeys: ['finances'],
     children: [
       // { name: 'Payouts', link: '/admin/finances/payouts' },
-      { name: 'Transactions', link: '/admin/finances/transactions' },
-      { name: 'Disputes', link: '/admin/finances/disputes' },
+      // { name: 'Transactions', link: '/admin/finances/transactions' },
+      // { name: 'Disputes', link: '/admin/finances/disputes' },
       { name: 'Beige credit points', link: '/admin/finances/creditPoints' },
       { name: 'CP Compensation', link: '/admin/finances/cpCompensation' },
 
