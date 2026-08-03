59a3d442087c1080c6c71187dabc1b4583e2b7e0
Punyashree Shetty <punyashree.shetty@gmail.com>
2026-08-03 19:35:03 +0530

    Press&Blog page: inital commit

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -146,6 +146,7 @@ const navLinks = [
   { label: "About", href: "/about" },
   { label: "Find Creative Work", href: "/find-creative-work" },
   { label: "Use Cases", href: "#usecases", hasDropdown: true },
+  { label: "Press & Blogs", href: "/press-blogs" },
 ];
 
 export const Navbar = () => {
